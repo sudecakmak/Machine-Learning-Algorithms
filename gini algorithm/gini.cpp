2e1e2f30f9f5eb318f783767351fd854b34bdda9
Sude Çakmak <sudecakmak46@gmail.com>
2023-03-23 00:14:19 +0300

    gini algorithm

---
+++ gini algorithm/gini.cpp
@@ -0,0 +1,78 @@
+#include <iostream>
+
+using namespace std;
+
+string Result(int Sex, int Status, int Kid);
+void Print(int Sex, int Status, int Kid);
+
+int main()
+{
+	int sex;
+	int status;
+	int kid;
+
+	while (1)
+	{
+		cout << "Enter number: (1-Female, 2-Male)" << endl;
+		cin >> sex;
+
+		cout << "Enter number: (1-Single, 2-Married, 3-Divorced)" << endl;
+		cin >> status;
+
+		cout << "Enter number: (1-Has kid, 2-No kid)" << endl;
+		cin >> kid;
+
+		cout << endl << "Ideal Customer = " << Result(sex, status, kid);
+		cout << endl << "--------------" << endl;
+		Print(sex, status, kid);
+		cout << "\n\n\n";
+	}
+
+}
+
+string Result(int Sex, int Status, int Kid) {
+
+	string idealCustomer;
+
+	if (Kid == 1)
+	{
+		idealCustomer = "No";
+	}
+	else if (Kid == 2)
+	{
+		if (Sex == 1)
+		{
+			idealCustomer = "Yes";
+		}
+		else if (Sex == 2)
+		{
+			if (Status == 1)
+				idealCustomer = "Yes";
+			else if (Status == 3)
+				idealCustomer = "No";
+		}
+	}
+
+	return idealCustomer;
+}
+
+void Print(int Sex, int Status, int Kid) {
+
+	if (Sex == 1)
+		cout << "Female" << endl;
+	else if (Sex == 2)
+		cout << "Male" << endl;
+
+	if (Status == 1)
+		cout << "Single" << endl;
+	else if (Status == 2)
+		cout << "Married" << endl;
+	else if (Status == 3)
+		cout << "Divorced" << endl;
+
+	if (Kid == 1)
+		cout << "Has kid" << endl;
+	else if (Kid == 2)
+		cout << "No kid" << endl;
+}
+
